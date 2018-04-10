8b3e855be6de9a9fb8111d9e11ff2de195f7ded9
sununiq <sunzhe@192.168.0.100>
2018-04-10 22:43:22 +0800

    add fpscala ch3 tree example

--- src/main/scala/com/sununiq/github/basic/hof/Chp3.scala
+++ src/main/scala/com/sununiq/github/basic/hof/Chp3.scala
@@ -90,5 +90,52 @@ object Chp3 {
     import List._
     val ls: List[Int] = List(1, 2, 3, 4, 5)
     println(dropWhile(ls, (x: Int) => x == 4))
+
   }
+}
+
+sealed trait Tree[+A]
+
+case class Leaf[A](value: A) extends Tree[A]
+
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+object Tree {
+
+  def size[A](tree: Tree[A]): Int = tree match {
+    case Leaf(_) => 1
+    case Branch(left, right) => 1 + size(left) + size(right)
+  }
+
+
+  def maximum(tree: Tree[Int]): Int = tree match {
+    case Leaf(value) => value
+    case Branch(left, right) => maximum(left) max maximum(right)
+  }
+
+  def depth[A](tree: Tree[A]): Int = tree match {
+    case Leaf(_) => 0
+    case Branch(left, right) => 1 + (depth(left) max depth(right))
+  }
+
+  def map[A, B](tree: Tree[A], f: A => B): Tree[B] = tree match {
+    case Leaf(value) => Leaf(f(value))
+    case Branch(left, right) => Branch(map(left, f), map(right, f))
+  }
+
+  /**
+    * 参考答案实现
+    */
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Leaf(value) => f(value)
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+  }
+
+  def size2[A](tree: Tree[A]): Int = fold(tree)(_ => 1)(1 + _ + _)
+
+  def maximum2(tree: Tree[Int]): Int = fold(tree)(a => a)(_ max _)
+
+  def depth2[A](tree: Tree[A]): Int = fold(tree)(a => 0)((d1, d2) => d1 max d2)
+
+  def map2[A,B](t: Tree[A])(f: A => B): Tree[B] = fold(t)(a => Leaf(f(a)): Tree[B])(Branch(_,_))
 }
